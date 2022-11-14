26e94b030ac30ee5dbf25e8f5efb917f6308c433
Raazkatwal <raazkatwal5@gmail.com>
2022-11-14 15:22:19 +0545

    added wasd controls

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ var x=0,y=0;
 const playable_width = window.screen.width - player.clientWidth; 
 const playable_height = window.screen.height - 170;
 document.addEventListener("keydown", e => {
-    //if up btn
-    if (e.keyCode==38) {
+    //if up or W btn
+    if (e.keyCode==38 || e.keyCode==87) {
         if (y!=0) {
             y-=10;
         }
         player.style.transform = `translate(${x}px, ${y}px)`;
     }
-    //if down btn
-    if (e.keyCode==40) {
+    //if down or S btn
+    if (e.keyCode==40 || e.keyCode==83) {
         if (y<playable_height) {
             y+=10;
         }
         player.style.transform = `translate(${x}px, ${y}px)`;
     }
-    //if right btn
-    if (e.keyCode==39) {
+    //if right or D btn
+    if (e.keyCode==39 || e.keyCode==68) {
         if (x<playable_width) {
             x+=10;
         }
         player.style.transform = `translate(${x}px, ${y}px)`;
     }
-    //if left btn
-    if (e.keyCode==37) {
+    //if left or A btn
+    if (e.keyCode==37 || e.keyCode==65) {
         if (x!=0) {
             x-=10;
         }
